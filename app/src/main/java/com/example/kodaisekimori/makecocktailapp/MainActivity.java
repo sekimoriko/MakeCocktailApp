5da49d87235c6e405bff91a94f09bc06cd9dd416
kodaisekimori <accelerator322@gmail.com>
2017-08-29 11:16:24 +0900

    startボタン作成、材料選択画面遷移

--- app/src/main/java/com/example/kodaisekimori/makecocktailapp/MainActivity.java
+++ app/src/main/java/com/example/kodaisekimori/makecocktailapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.kodaisekimori.makecocktailapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //スタートボタン
+        Button startButton = (Button)findViewById(R.id.start_button);
+
+        //次に遷移
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
